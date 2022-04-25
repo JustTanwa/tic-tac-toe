b0a2edafb701fd3a2cd11d3d9f6171fd5367b581
Katakung95 <tanwasripan@hotmail.co.uk>
2022-04-25 20:01:43 +0100

    added how to play button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from "react";
-import "./App.css";
-import Game from "./components/Game";
+import React from 'react';
+import './App.css';
+import Game from './components/Game';
+import Help from './components/Help';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="text-center"><h1 className="title">Tic-Tac-Toe</h1></header>
-      <Game />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header>
+				<Help />
+				<h1 className='title'>Tic-Tac-Toe</h1>
+			</header>
+			<Game />
+			<footer>&copy; 2022 Tanwa Sripan</footer>
+		</div>
+	);
 }
 
 export default App;
